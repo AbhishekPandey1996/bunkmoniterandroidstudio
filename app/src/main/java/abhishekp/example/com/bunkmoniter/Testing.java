458626f308ffd745ae58f9429a07fe25293c3a9d
prayasjain <prayas.jain.cse14@iitbhu.ac.in>
2015-07-11 22:52:42 +0530

    Update-layout

--- app/src/main/java/abhishekp/example/com/bunkmoniter/Testing.java
+++ app/src/main/java/abhishekp/example/com/bunkmoniter/Testing.java
@@ -2,6 +2,7 @@ package abhishekp.example.com.bunkmoniter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
